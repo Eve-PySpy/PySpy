ad5f675a09a4d46833a3aad5f70989a4db691793
Crypta Eve <crypta@crypta.tech>
2019-09-05 13:51:09 +1000

    Change double click behaviour to be more relevant to the information that was clicked on.
    For example clicking on the Corporation Column will open the zkillboard page for the corporation instead of the player as previous.

--- gui.py
+++ gui.py
@@ -615,11 +615,59 @@ class Frame(wx.Frame):
 
     def _goToZKill(self, event):
         rowidx = event.GetRow()
-        character_id = self.options.Get("outlist")[rowidx][0]
-        webbrowser.open_new_tab(
-            "https://zkillboard.com/character/" +
-            str(character_id) + "/"
-            )
+        colidx = event.GetCol()
+
+        url = "https://zkillboard.com/"
+
+        # Corporation was clicked on, link to that killboard
+        if colidx == 5:
+            corporation_id = self.options.Get("outlist")[rowidx][3]
+            url = url + "corporation/" + str(corporation_id) + "/"
+
+        # Alliance was clicked on, link to that killboard if alliance exists
+        elif colidx == 7:
+            alliance_id = self.options.Get("outlist")[rowidx][5]
+            if alliance_id != None:
+                url = url + "alliance/" + str(alliance_id) + "/"
+
+        # Faction was clicked on, link to that killboard if faction exists
+        elif colidx == 8:
+            faction_id = self.options.Get("outlist")[rowidx][1]
+            if faction_id != None:
+                url = url + "faction/" + str(faction_id) + "/"
+
+        # Something other than character was clicked on but we want to look at the character with modifiers
+        elif colidx != 2:
+            # Set up the character base url
+            character_id = self.options.Get("outlist")[rowidx][0]
+            url = url + "character/" + str(character_id) + "/"
+
+            # Kills modifier
+            if colidx == 9:
+                url = url + "kills/"
+            # Losses modifier
+            elif colidx == 10:
+                url = url + "losses/"
+            # Solo Modifier
+            elif colidx == 12:
+                url = url + "solo/"
+            # BLOPS Modifer
+            elif colidx == 13:
+                url = url + "group/898/"
+            # HIC Modifier
+            elif colidx == 14:
+                url = url + "group/894/"
+            # Abyssal Modifier
+            elif colidx:
+                url = url + "abyssal/"
+
+        # This is a catch all if the url wasnt set or if a column other than a special one was clicked.
+        # This is in a seperate flow to the above in case any of the above need to fall through
+        if url == "https://zkillboard.com/":
+            character_id = self.options.Get("outlist")[rowidx][0]
+            url = url + "character/" + str(character_id) + "/"
+
+        webbrowser.open_new_tab(url)
 
     def _showContextMenu(self, event):
         '''
